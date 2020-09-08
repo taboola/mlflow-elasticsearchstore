dbf2d899801e804e2134c6499e2f4b0ab152b2b3
Axel Vivien <a.vivien@criteo.com>
2020-09-08 15:49:40 +0200

    fix integration tests

--- tests/scripts/initialize_database.sh
+++ tests/scripts/initialize_database.sh
@@ -5,7 +5,8 @@ docker images
 
 docker build  --tag elastic_store .
 docker run \
-    --network=host \
+    -p 9200:9200 \
+    -p 9300:9300 \
     -e "discovery.type=single-node" \
     -e ELASTIC_PASSWORD=password \
     -e xpack.security.enabled=true \
